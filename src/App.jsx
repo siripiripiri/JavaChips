9750e89c9b2ce7ea1ad006ca3ac16a210505b009
Daylen McCarron <daylenricardomccarron@gmail.com>
2024-01-26 18:24:11 +0530

    Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import LoginForm from "./components/LoginForm";
-import SignUpForm from "./components/SignUpForm";
 
 function App() {
   return (
@@ -10,8 +9,7 @@ function App() {
         <Link to="signup">Sign-Up</Link>
       </header>
       <Routes>
-        <Route path="login" element={<LoginForm/>} />    
-        <Route path="signup" element={<SignUpForm/>} />  
+        <Route path="login" element={<LoginForm/>} /> 
         <Route path="*" element={
           <>
           <h1>404</h1>
