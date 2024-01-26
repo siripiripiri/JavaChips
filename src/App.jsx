01fe1a12adb14f44acd50bdec436c90d4f0d91af
Rohan Jaiswal <rj957977@gmail.com>
2024-01-26 21:13:20 +0530

    fun stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import LoginForm from "./components/LoginForm";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <BrowserRouter>
       <header>
         <Link to="login">Login</Link>
-        <Link to="signup">Sign-Up</Link>
+        <Link to="profile">Profile</Link>
       </header>
       <Routes>
         <Route path="login" element={<LoginForm/>} /> 
+        <Route path="profile" element={<Profile/>} />
         <Route path="*" element={
           <>
           <h1>404</h1>
